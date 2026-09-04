ebfe3f00318e0c41ff4d5437204f81a33d234047
Daniel J. McGrath <djmcgrath@cybertec.io>
2026-09-03 21:11:41 -0500

    build(tools): create nx-utils project

---
+++ tools/nx/utils/src/lib/nx-utils.spec.ts
@@ -0,0 +1,7 @@
+import { nxUtils } from './nx-utils';
+
+describe('nxUtils', () => {
+  it('should work', () => {
+    expect(nxUtils()).toEqual('nx-utils');
+  });
+});
---
+++ tools/nx/utils/src/lib/nx-utils.ts
@@ -0,0 +1,3 @@
+export function nxUtils(): string {
+  return 'nx-utils';
+}
