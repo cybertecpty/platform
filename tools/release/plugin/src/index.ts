8ac50f4ac3d574183f644e561938d60097ad1b01
Daniel J. McGrath <djmcgrath@cybertec.io>
2026-09-10 16:28:23 -0500

    feat(release-plugin): add package-version generator

--- tools/release/plugin/src/index.ts
+++ tools/release/plugin/src/index.ts
@@ -1,3 +1,5 @@
+export { default as packageVersionGenerator } from './lib/generators/package-version/generator';
+export type { PackageVersionGeneratorOptions } from './lib/generators/package-version/schema';
 export { default as releaseManifestGenerator } from './lib/generators/release-manifest/generator';
 export type { ReleaseManifestGeneratorOptions } from './lib/generators/release-manifest/schema';
 export { default as releaseGenerator } from './lib/generators/release/generator';
